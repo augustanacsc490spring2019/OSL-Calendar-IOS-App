b7c48ff26b3882bb65b11f9c75482c3ab567cc0e
Kyle Workman <kyleworkman@Kyles-MacBook-Pro-2.local>
2019-03-26 10:43:51 -0500

    Starting iOS Client App

---
+++ OSL-Calendar-IOS-App/SearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewController.swift
+//  OSL-Calendar-IOS-App
+//
+//  Created by Kyle Workman on 3/26/19.
+//  Copyright © 2019 Kyle Workman. All rights reserved.
+//
+
+import Foundation
